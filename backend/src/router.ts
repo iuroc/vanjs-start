0d52e5c7cda00ab0a4f5c1563609326011bed455
iuroc <m@iuroc.com>
2024-03-27 12:41:18 +0800

    ...

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
 import demo from './route/demo'
+import { join } from 'path'
 
 const router = Router()
 
+router.get('/', (_, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'))
+})
 router.use('/api/demo', demo)
+// 这里可以创建更多路由
+// 这里可以创建更多路由
+// 这里可以创建更多路由
 
 export default router
